59ec756d56cfbabd64d10502f22e77d5a44624bc
dre <spinninghalf@gmail.com>
2018-08-14 16:20:00 +1000

    parse buffer for escpos cut operation. save to file if found

--- listen.js
+++ listen.js
@@ -1,10 +1,24 @@
 const SERIAL_PORT = '/dev/tty.usbserial'
 const SerialPort = require('serialport');
 const fs = require('fs');
+const orderParser = require('./parser');
+const colors = require('colors');
 
 const port = new SerialPort(SERIAL_PORT);
-var ESCPOS_DATA_FILENAME = './escpos-data.bin';
+var ESCPOS_DATA_FILENAME = 'escpos-data.bin';
+var myBuffer = Buffer.alloc(0);
+MAX_BUFFER_SIZE = 10192;
 
+// ESC/POS op code for 'select cut mode and cut paper'
+// ASCII GS V m=0 => make a full cut
+// hex   1d 56 00 
+// dec   29 86 00
+// this will change for different cut options on
+// POS systems => need to get a hexdump of their bytes
+// to see exactly what escpos op they're using.
+//
+// this creates a buffer containing [0x1d, 0x56, 0x00]
+const PAPER_CUT_OP_BUFFER = Buffer.from([29,86,00])  
 
 port.on('error', function (err) {
     console.log('ERROR: ', err.message);
@@ -16,16 +30,64 @@ port.on('open', function (err) {
     }
 });
 
+// switches the port into 'flowing mode'
 port.on('data', function (data) {
-    console.log('DATA: ', data.toString('utf8'));
-    try {
-        fs.appendFileSync(ESCPOS_DATA_FILENAME, data);
-        console.log('SUCCESS: appended data to ', ESCPOS_DATA_FILENAME);
-    } catch (e) {
-        console.log('ERROR appending data to ', ESCPOS_DATA_FILENAME);
-        
+    //console.log('DATA: ', data);
+    //console.log('DATA: ', data.toString('utf8'));
+    //console.log('myBuffer.length: ', myBuffer.length);
+    //console.log(myBuffer);
+
+    myBuffer = Buffer.concat([myBuffer, data])
+
+    if ( myBuffer.includes(PAPER_CUT_OP_BUFFER) ) {
+        cut_location = myBuffer.indexOf(PAPER_CUT_OP_BUFFER);
+        console.log('found a paper cut op...'.green);
+        console.log('cut_location: '
+                    , cut_location
+                    , ' myBuffer.length: '
+                    , myBuffer.length
+        )
+
+        const op_endpt = cut_location + PAPER_CUT_OP_BUFFER.length + 1;
+
+        saveBufferToFile(myBuffer.slice(0, op_endpt));
+
+        if (myBuffer.length > op_endpt) {
+            //have additional bytes on end from a new order.
+            // => don't thrwo them away !
+            console.log('WARNING:'.red);
+            console.log('myBuffer contains extra bytes from new order, after cut op'.red);
+            console.log('...not throwing them away => add to start of new buffer!'.red)
+            myBuffer = myBuffer.slice(op_endpt + 1, myBuffer.length);
+        } else {
+            resetMyBuffer();
+        }
+      
+    }
+    
+    if (myBuffer.length >= MAX_BUFFER_SIZE) {
+         // TODO
+        //saveBufferToFile ();
+        console.log('TODO: hit or exceeded MAX_BUFFER_SIZE => need to implement a save')
     }
 });
+
+function saveBufferToFile (buff) {
+    try {
+        console.log('=> writing buffer to file:', ESCPOS_DATA_FILENAME);
+        fs.appendFileSync(ESCPOS_DATA_FILENAME, buff);
+        console.log('SUCCESSFULLY wrote buff to file');
+    } catch (e) {
+        console.log('ERROR appending buff to ',
+                     ESCPOS_DATA_FILENAME,
+                     e.message);
+    }
+}
+
+function resetMyBuffer () {
+    myBuffer = Buffer.alloc(0);
+}
+
 
 port.on('close', function (err) {
     if (err) {
--- parser.js
+++ parser.js
@@ -2,7 +2,8 @@ var fs = require('fs');
 var _ = require('lodash');
 var npos = require('npos');
 var parser = npos.parser();
-var buffer = fs.readFileSync('escpos-data.bin');
+OUTPUT_FILENAME = 'escpos-data.bin';
+
 
 var DOCKET_START_FIELDS = [
   "RESTAURANT BAR",
@@ -12,71 +13,79 @@ var DOCKET_START_FIELDS = [
   "BOTTLESHOP",
 ];
 
+orderParser(OUTPUT_FILENAME);
 
-parser.parse(buffer).then(function(ast) {
-  //console.log(ast);
+function orderParser (fileName) {
+  var buffer = fs.readFileSync(fileName);
 
-  npos.textualize(ast).then(function (results) {
-    // console.log(results);
+  parser.parse(buffer).then(function(ast) {
+    //console.log(ast);
 
-    // create a copy of results array
-    var data = _.slice(results);
+    npos.textualize(ast).then(function (results) {
+      // console.log(results);
 
-    var cleanedData_1 = _.reject(data, (o) => {
-      return (
-        o === '' || o === '\n'
-      );
-    });
+      // create a copy of results array
+      var data = _.slice(results);
 
-    var cleanedData_2 = _.map(cleanedData_1, (o) => {
-      var temp;
-      if (_.last(o) === '\n') {
-        temp =  _.slice(o,0, o.length-1)
-      } else {
-        temp = o;
-      }
+      var cleanedData_1 = _.reject(data, (o) => {
+        return (
+          o === '' || o === '\n'
+        );
+      });
 
-      return temp.join("").toUpperCase();
-    });
-
-
-    var docketStartLocations = [];
-
-    // find start locations of orders
-     _.forEach(DOCKET_START_FIELDS, (field) => {
-      //console.log(field);
-      for (var i =0; i <= cleanedData_2.length; i++) {
-        // console.log( cleanedData_2[i]);
-        if (cleanedData_2[i] === field) {
-          // console.log(field, 'found start at: ', i);
-          docketStartLocations = _.concat(docketStartLocations, [i]);
+      var cleanedData_2 = _.map(cleanedData_1, (o) => {
+        var temp;
+        if (_.last(o) === '\n') {
+          temp =  _.slice(o,0, o.length-1)
+        } else {
+          temp = o;
         }
-      }
+
+        return temp.join("").toUpperCase();
+      });
+
+
+      var docketStartLocations = [];
+
+      // find start locations of orders
+       _.forEach(DOCKET_START_FIELDS, (field) => {
+        //console.log(field);
+        for (var i =0; i <= cleanedData_2.length; i++) {
+          // console.log( cleanedData_2[i]);
+          if (cleanedData_2[i] === field) {
+            // console.log(field, 'found start at: ', i);
+            docketStartLocations = _.concat(docketStartLocations, [i]);
+          }
+        }
+      });
+
+
+      // _.orderBy with no iteratees arg will sort by ascending order by default
+      var sortedDocketStartLocations = _.orderBy(docketStartLocations);
+
+      // console.log(cleanedData_2);
+
+      var orders = _.reduce(sortedDocketStartLocations, (acc, val, index, coll) => {
+        if (index === coll.length) {
+          var order = _.slice(cleanedData_2, sordedDocketStartLocations[index], cleanedData_2.length); 
+          return _concat(acc, [order]);
+        } else {
+          var order = _.slice(cleanedData_2,sortedDocketStartLocations[index],sortedDocketStartLocations[index + 1]);
+          return _.concat(acc, [order]);
+        }
+      }, []);
+
+      // console.log(orders);
+
+      var orderData = {};
+      orderData['orders'] = orders;
+
+      console.log('NOT sorted docket start locations: ', docketStartLocations);
+      console.log('sortedDocketStartLocations: ', sortedDocketStartLocations);
+      console.log(JSON.stringify(orderData, null,2));
     });
-
-
-    // _.orderBy with no iteratees arg will sort by ascending order by default
-    var sortedDocketStartLocations = _.orderBy(docketStartLocations);
-
-    // console.log(cleanedData_2);
-
-    var orders = _.reduce(sortedDocketStartLocations, (acc, val, index, coll) => {
-      if (index === coll.length) {
-        var order = _.slice(cleanedData_2, sordedDocketStartLocations[index], cleanedData_2.length); 
-        return _concat(acc, [order]);
-      } else {
-        var order = _.slice(cleanedData_2,sortedDocketStartLocations[index],sortedDocketStartLocations[index + 1]);
-        return _.concat(acc, [order]);
-      }
-    }, []);
-
-    // console.log(orders);
-
-    var orderData = {};
-    orderData['orders'] = orders;
-
-    console.log('NOT sorted docket start locations: ', docketStartLocations);
-    console.log('sortedDocketStartLocations: ', sortedDocketStartLocations);
-    console.log(JSON.stringify(orderData, null,2));
   });
-});
+
+}
+
+exports.orderParser = orderParser;
