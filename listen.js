7365d1a942ccda7ddd1d83f325c7dfa97a581101
dre <spinninghalf@gmail.com>
2018-08-14 18:21:38 +1000

    wip: max buffer size saving

--- listen.js
+++ listen.js
@@ -1,13 +1,13 @@
 const SERIAL_PORT = '/dev/tty.usbserial'
 const SerialPort = require('serialport');
 const fs = require('fs');
-const orderParser = require('./parser');
+// const orderParser = require('./parser');
 const colors = require('colors');
 
 const port = new SerialPort(SERIAL_PORT);
 var ESCPOS_DATA_FILENAME = 'escpos-data.bin';
 var myBuffer = Buffer.alloc(0);
-MAX_BUFFER_SIZE = 10192;
+MAX_BUFFER_SIZE = 2048;
 
 // ESC/POS op code for 'select cut mode and cut paper'
 // ASCII GS V m=0 => make a full cut
@@ -39,38 +39,66 @@ port.on('data', function (data) {
 
     myBuffer = Buffer.concat([myBuffer, data])
 
-    if ( myBuffer.includes(PAPER_CUT_OP_BUFFER) ) {
-        cut_location = myBuffer.indexOf(PAPER_CUT_OP_BUFFER);
-        console.log('found a paper cut op...'.green);
-        console.log('cut_location: '
-                    , cut_location
-                    , ' myBuffer.length: '
-                    , myBuffer.length
-        )
-
-        const op_endpt = cut_location + PAPER_CUT_OP_BUFFER.length + 1;
-
-        saveBufferToFile(myBuffer.slice(0, op_endpt));
-
-        if (myBuffer.length > op_endpt) {
-            //have additional bytes on end from a new order.
-            // => don't thrwo them away !
-            console.log('WARNING:'.red);
-            console.log('myBuffer contains extra bytes from new order, after cut op'.red);
-            console.log('...not throwing them away => add to start of new buffer!'.red)
-            myBuffer = myBuffer.slice(op_endpt + 1, myBuffer.length);
-        } else {
-            resetMyBuffer();
-        }
-      
-    }
-    
     if (myBuffer.length >= MAX_BUFFER_SIZE) {
-         // TODO
-        //saveBufferToFile ();
-        console.log('TODO: hit or exceeded MAX_BUFFER_SIZE => need to implement a save')
+        //prepare to save buffer to file, but first check to see if
+        //it contains the paper cut operator. if it does, just save
+        //the order and keep and remaining bytes after it.
+        //otherwise, save the whole buffer and upon successful save,
+        //reset it.
+        console.log('hit/exceeded MAX_BUFFER_SIZE => checking for cut op'.red);
+
+        if (checkForCutOperator(myBuffer)) {
+            cut_location = myBuffer.indexOf(PAPER_CUT_OP_BUFFER);
+            const op_endpt = cut_location + PAPER_CUT_OP_BUFFER.length;
+            saveBufferToFile(myBuffer.slice(0, op_endpt));
+
+            console.log('found a paper cut op...'.green);
+            console.log('cut_location: '
+                        , cut_location
+                        , ' myBuffer.length: '
+                        , myBuffer.length)
+
+            if (checkForExtraBytes(myBuffer, op_endpt)) {
+                // keep the extra bytes
+                myBuffer = myBuffer.slice(op_endpt, myBuffer.length);
+            } else {
+                // no extra bytes. should be safe to reset buff to blank
+                myBuffer = Buffer.alloc(0);
+            }
+        } else {
+            // max buffer size reached, no paper cut operator.
+            // => save to file and reset buffer to blank
+            console.log('no cut op found. saving entire buffer, then resetting it');
+            saveBufferToFile(myBuffer);
+            myBuffer = Buffer.alloc(0);
+        } 
+    } else {
+      // saveBufferToFile(data);
     }
 });
+
+
+function checkForExtraBytes(buff, op_endpt) {
+    //may have additional bytes on end after op_endpt from a new order.
+    // => if so don't thow them away! make these bytes the start of next order.
+    if (buff.length > op_endpt) {
+        console.log('WARNING:'.red);
+        console.log('myBuffer contains extra bytes from new order, after cut op'.red);
+        console.log('...not throwing them away => add to start of new buffer!'.red)
+        return true;
+    } else {
+        //no extra bytes after op_endpt
+        return false;
+    }
+}
+
+function checkForCutOperator(buff) {
+    if ( buff.includes(PAPER_CUT_OP_BUFFER) ) {
+        return true;
+    } else {
+        return false
+    }
+}
 
 function saveBufferToFile (buff) {
     try {
@@ -83,11 +111,6 @@ function saveBufferToFile (buff) {
                      e.message);
     }
 }
-
-function resetMyBuffer () {
-    myBuffer = Buffer.alloc(0);
-}
-
 
 port.on('close', function (err) {
     if (err) {
