edebf0a66172f941a22c185558835751c6778e6d
dre <spinninghalf@gmail.com>
2018-08-18 16:24:05 +1000

    wip: hacky implementation of properly handling metaData of order and its potential extra variable lines of content

--- parser.js
+++ parser.js
@@ -116,59 +116,12 @@ function splitIntoSingleOrders(data) {
 }
 
 function orderToObjectLiteral (order) {
-  //order will be an array of strings
-  //
-  // IMPORTANT: DOCKET SPECIFICATIONS
-  // the following is the result of looking at many physical dockets and inferring the
-  // structure. for various edge cases the structure changes slightly, and this 
-  // affects how we should handle parsing the lines into relevant fields required
-  // by db schema expectations.
-  //
-  // below, the lines refer to lines where text IS (we neglect the presence of empty
-  // lines in the commentary)
-  //
-  // A) ALWAYS have
-  // LINE 1: location e.g. "SPORTS BAR"
-  // LINE 2: what device took the order e.g. "Table 4"
-  // LINE 3: person who took the order e.g. "Clerk: Marlene"
-  // LINE 4: when then order was taken e.g. "17/08/2018 17:24:04"
-  //
-  // B) VARIABLE content
-  //
-  // LINE 4: most times has table num/order number. MAY NOT BE PRESENT!(i.e. a staff meal)
-  // LINE 5: booking name/walk-in: MAY NOT BE PRESENT
-  // LINE 6: Covers number: MAY NOT BE PRESENT
-  // LINE 7: extra weird info: "PRINT A/C - SARAH @ 19:11"
-  //
-  // C) ALWAYS have AT LEAST ONE docket course field.
-  // LINE 8: docket course field (underlined field on phys docket) e.g. ENTREES DINNER
-  // LINE 9+: meal item(s)
-  //
-  // TODO: error handling of string/regex extraction methods, operating on an assumed
-  // non-empty array, order.
-  //
-  // IMPLEMENTATION TACTIC:
-  // assume that A) above always exists, so it's valid to extract the first 4 lines:
-  // LINE 1
-  const location = order[0];
-  // LINE 2
-  const orderTakenUsing = order[1];
-  // LINE 3
-  // assume always of form "Clerk: Harry"
-  const clerk = (order[2]).slice(order[2].indexOf(':')+2); 
-  // LINE 4
-  const orderSentAt = order[3];
-  
-  // due to B) variances we need to scan forward to our dependable docket course field.
-  // once we find that, we can safely look at lines:
-  //     LINE 4 < *lines* < LINE course field index
-  // then the *lines* will contain our variable content which needs to be stepped though
-  // carefully during our parsing.
-
-  // variable content (!). careful.
-  const tableNumber = order[4].split(/\s+/).slice(-1)[0]; // "Order no *20/0*"
-  const customerName = (order[5]).slice(order[5].indexOf(':')+2);
-  const covers = (order[6]).slice(order[6].indexOf(':')+2);
+  //order will be an array of strings. but before going further check that the array
+  //has minimal expected content. namely: area, orderTakenUsing, clerk, orderSentAt.
+  // otherwise throw error, for now.
+  if (order.length < 4) {
+    throw Error('ERROR: order array is below minimal expected length. parse error...');
+  }
 
   // put basic templete here for now
   var template = {
@@ -189,19 +142,123 @@ function orderToObjectLiteral (order) {
     }
   };
 
+  // ****************************************
+  // IMPORTANT: DOCKET SPECIFICATIONS
+  // ****************************************
+  // the following is the result of looking at many physical dockets and inferring the
+  // structure. for various edge cases the structure changes slightly, and this 
+  // affects how we should handle parsing the lines into relevant fields required
+  // by db schema expectations.
+  //
+  // below, the lines refer to lines where text IS (we neglect the presence of empty
+  // lines in the commentary)
+  //
+  // A) ALWAYS have
+  // LINE 1: location e.g. "SPORTS BAR"
+  // LINE 2: what device took the order e.g. "Table 4"
+  // LINE 3: person who took the order e.g. "Clerk: Marlene"
+  // LINE 4: when then order was taken e.g. "17/08/2018 17:24:04"
+
+  // B) VARIABLE content
+  // LINE 4: most times has table num/order number. MAY NOT BE PRESENT!(i.e. a staff meal)
+  // LINE 5: booking name/walk-in: MAY NOT BE PRESENT
+  // LINE 6: Covers number: MAY NOT BE PRESENT
+  // LINE 7: extra weird info: "PRINT A/C - SARAH @ 19:11"
+
+  // C) ALWAYS have AT LEAST ONE docket course field.
+  // LINE 8: docket course field (underlined field on phys docket) e.g. ENTREES DINNER
+  // LINE 9+: meal item(s)
+
+  // TODO: error handling of string/regex extraction methods,
+
+  // START
+  // IMPLEMENTATION TACTIC:
+  // assume that A) above always exists, so it's valid to extract the first 4 lines:
+  // LINE 1
+  const location = order[0];
+  // LINE 2
+  const orderTakenUsing = order[1];
+  // LINE 3
+  // assume always of form "Clerk: Harry"
+  const clerk = (order[2]).slice(order[2].indexOf(':')+2); 
+  // LINE 4
+  const orderSentAt = order[3];
+  
+  // NEXT UP
+  // due to B) variances we need to scan forward to our dependable docket course field.
+  // once we find that, we can safely look at lines:
+  //     LINE 4 < *lines* < LINE course field index
+  // then the *lines* will contain our variable content which needs to be stepped though
+  // carefully during our parsing.
+
+  // be verbose:
+  const variableContentStart = 4;
+
+  // trimmedLocations is like [ val, val, ..., val]
+  // where val is like ['MAINS DINNER', [12] ]
+  // ==> 12 is the index of MAINS DINNER in 'order' variable.
+  const trimmedLocations = getAllCourseFieldsLocations(order);
+  var firstCourseField;
+  if (trimmedLocations.length > 0) {
+    firstCourseField = trimmedLocations[0][1][0];
+  } else {
+    throw Error('ERROR: firstCourseField cannot be found in trimmedLocations array');
+  }
+
+  const variableContent = order.slice(variableContentStart, firstCourseField);
+  console.log('variableContent: '.red, variableContent);
+   
+  // now look at each element of variableContent, searching for existence of substrings
+  // 1. "TABLE No *15/0*
+  // 2. "ORDER NUMBER 1"
+  // 3. "Name: Walk-in"
+  // 4. "Covers: 10"
+  // 5. other extra weird info: "PRINT A/C - SARAH @ 19:11"
+
+  var extraContent = handleExtraVariableContent(variableContent);
+
+  var tableNumber;
+  const vTableNumber = _.find(variableContent, line => {
+    const lineUpper = line.toUpperCase();
+    return (lineUpper.includes("TABLE NO") || lineUpper.includes("ORDER NUMBER"));
+  });
+  if (vTableNumber) {
+    // handle the two different cases similarly.
+    // "TABLE No *20/0*" ==> "*20/0*"
+    // "ORDER NUMBER 1" ==> "1"
+    tableNumber = vTableNumber.split(/\s+/).slice(-1)[0];
+  }
+
+  var customerName;
+  const vCustomerName = _.find(variableContent, line => {
+    const nameUpper = line.toUpperCase();
+    return nameUpper.includes("NAME:");
+  });
+  if (vCustomerName) {
+    // "Name: Hillary"
+    customerName = vCustomerName.slice(vCustomerName.indexOf(':') + 2);
+  }
+
+  var covers;
+  const vCovers = _.find(variableContent, line => {
+    const coversUpper = line.toUpperCase();
+    return coversUpper.includes("COVERS:");
+  });
+  if (vCovers) {
+    // "Covers: 10"
+    covers = vCovers.slice(vCovers.indexOf(':') + 2);
+  }
+
   template.area = location;
   template.metaData.orderTakenUsing = orderTakenUsing;
   template.metaData.clerk = clerk;
   template.metaData.orderSentAt = orderSentAt;
+  template.metaData.extraContent = extraContent;
   template.tableNumber = tableNumber;
   template.customerName = customerName;
   template.covers = covers;
 
-  // now need todo more work to get the meals object correctly segmented...
-  // onwards.
-  const trimmedLocations = getAllCourseFieldsLocations(order);
-
-  // start building out the meal
+  // start building out the actual meal contents.
   // go thru trimmedLocations and take slices from 'order' variable, for
   // a given course field
   const meals = buildOutMeals(order, trimmedLocations);
@@ -209,6 +266,60 @@ function orderToObjectLiteral (order) {
   dbHandler.insertSingleOrder(template);
 }
 
+function handleExtraVariableContent(variableContent) {
+  //----------------------------------------
+  // REALLY CRAPPY CODE => it's working but need to refactor it. later
+  //----------------------------------------
+  // this is used to check for super weird extra variable content (pt 5 immediatley
+  // above)
+  const variableContentKeys = [
+    "NAME:",
+    "TABLE NO",
+    "ORDER NUMBER",
+    "COVERS:",
+  ];
+
+  var standardContentIndices = _.map(variableContentKeys, (key) => {
+    var _index;
+    const res = _.find(variableContent, (val, index) => {
+      _index = index;
+      return val.toUpperCase().includes(key);
+    });
+    if (res) {
+      return _index;
+    }
+  });
+
+  console.log('standardContentIndices: ', standardContentIndices);
+  //remove any undefined elements
+  standardContentIndices = _.reject(standardContentIndices, val => {
+    return val === undefined;
+  });
+  console.log('standardContentIndices: ', standardContentIndices);
+
+  var variableContentCopy = variableContent.slice();
+
+  _.forEach(standardContentIndices, val => {
+    delete variableContentCopy[val];
+  });
+  var extraContent = _.reject(variableContentCopy, val => {
+    return val === undefined;
+  });
+
+  if (_.isEmpty(extraContent)) {
+    extraContent = '';
+    console.log('no extraContent');
+  } else {
+    extraContent = extraContent.join(' ');
+    console.log('we have extra content: ', extraContent);
+  }
+  return extraContent;
+}
+
+// TODO: handle per item specific requests, the SUBLINES
+// LINE 1:    "3    PORTERHOUSE 200
+// SUBLINE 1: "M/R, jus, sal only"
+// SUBLINE 2: "--------------------"
 function buildOutMeals (order, trimmedLocations) {
   var meals = {};
   _.forEach(trimmedLocations,(val, index, coll) => {
