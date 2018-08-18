a5d038346d2c78c52945500734c3e9ee6d99b6ed
dre <spinninghalf@gmail.com>
2018-08-18 13:43:08 +1000

    refactor parser.js ==> orderToObjectLiteral function and write down docket specifications

--- parser.js
+++ parser.js
@@ -9,10 +9,11 @@ const colors = require('colors');
 const dbHandler = require('./dbHandler');
 const DOCKET_START_FIELDS = [
   "RESTAURANT BAR",
-  "JUKE BAR",
   "TAB BAR",
-  "GAMING BAR", // TODO: check this name using actual dockets
+  "JUKE BAR",
+  "GAMING BAR", 
   "BOTTLESHOP",
+  "SPORTS BAR",
 ];
 const DOCKET_COURSE_FIELDS = [
   "ENTREES DINNER",
@@ -117,14 +118,55 @@ function splitIntoSingleOrders(data) {
 function orderToObjectLiteral (order) {
   //order will be an array of strings
   //
-  //specs
-  //1. first element of orders will always be the location
-  //2. always have 3 lines in the docket for some of the meta data
+  // IMPORTANT: DOCKET SPECIFICATIONS
+  // the following is the result of looking at many physical dockets and inferring the
+  // structure. for various edge cases the structure changes slightly, and this 
+  // affects how we should handle parsing the lines into relevant fields required
+  // by db schema expectations.
+  //
+  // below, the lines refer to lines where text IS (we neglect the presence of empty
+  // lines in the commentary)
+  //
+  // A) ALWAYS have
+  // LINE 1: location e.g. "SPORTS BAR"
+  // LINE 2: what device took the order e.g. "Table 4"
+  // LINE 3: person who took the order e.g. "Clerk: Marlene"
+  // LINE 4: when then order was taken e.g. "17/08/2018 17:24:04"
+  //
+  // B) VARIABLE content
+  //
+  // LINE 4: most times has table num/order number. MAY NOT BE PRESENT!(i.e. a staff meal)
+  // LINE 5: booking name/walk-in: MAY NOT BE PRESENT
+  // LINE 6: Covers number: MAY NOT BE PRESENT
+  // LINE 7: extra weird info: "PRINT A/C - SARAH @ 19:11"
+  //
+  // C) ALWAYS have AT LEAST ONE docket course field.
+  // LINE 8: docket course field (underlined field on phys docket) e.g. ENTREES DINNER
+  // LINE 9+: meal item(s)
+  //
+  // TODO: error handling of string/regex extraction methods, operating on an assumed
+  // non-empty array, order.
+  //
+  // IMPLEMENTATION TACTIC:
+  // assume that A) above always exists, so it's valid to extract the first 4 lines:
+  // LINE 1
   const location = order[0];
+  // LINE 2
   const orderTakenUsing = order[1];
-  const clerk = (order[2]).slice(order[2].indexOf(':')+2);
-  const orderSent = order[3];
-  const tableNumber = order[4].split(/\s+/).slice(-1)[0];
+  // LINE 3
+  // assume always of form "Clerk: Harry"
+  const clerk = (order[2]).slice(order[2].indexOf(':')+2); 
+  // LINE 4
+  const orderSentAt = order[3];
+  
+  // due to B) variances we need to scan forward to our dependable docket course field.
+  // once we find that, we can safely look at lines:
+  //     LINE 4 < *lines* < LINE course field index
+  // then the *lines* will contain our variable content which needs to be stepped though
+  // carefully during our parsing.
+
+  // variable content (!). careful.
+  const tableNumber = order[4].split(/\s+/).slice(-1)[0]; // "Order no *20/0*"
   const customerName = (order[5]).slice(order[5].indexOf(':')+2);
   const covers = (order[6]).slice(order[6].indexOf(':')+2);
 
@@ -150,14 +192,47 @@ function orderToObjectLiteral (order) {
   template.area = location;
   template.metaData.orderTakenUsing = orderTakenUsing;
   template.metaData.clerk = clerk;
-  template.metaData.orderSent = orderSent;
+  template.metaData.orderSentAt = orderSentAt;
   template.tableNumber = tableNumber;
   template.customerName = customerName;
   template.covers = covers;
 
   // now need todo more work to get the meals object correctly segmented...
   // onwards.
+  const trimmedLocations = getAllCourseFieldsLocations(order);
 
+  // start building out the meal
+  // go thru trimmedLocations and take slices from 'order' variable, for
+  // a given course field
+  const meals = buildOutMeals(order, trimmedLocations);
+  template.meals = meals;
+  dbHandler.insertSingleOrder(template);
+}
+
+function buildOutMeals (order, trimmedLocations) {
+  var meals = {};
+  _.forEach(trimmedLocations,(val, index, coll) => {
+    const currentCourse = val[0];
+    const currentCourseFieldIndex = val[1][0];
+    var items;
+    if (index === trimmedLocations.length - 1) {
+      items = order.slice(currentCourseFieldIndex + 1, order.length); 
+    } else {
+      const nextCourseFieldIndex = trimmedLocations[index+1][1][0];
+      items = order.slice(currentCourseFieldIndex + 1, nextCourseFieldIndex); 
+    }
+    const parsedItems = _.map(items, item => {
+      var splitItem = item.split(/\s+/); // ["3","porterhouse","200"]
+      var itemQuantity = splitItem[0];
+      var itemName = splitItem.slice(1,splitItem.length).join(' ');
+      return {quantity: itemQuantity, item: itemName};
+    });
+    meals[currentCourse] = parsedItems;
+  });
+  return meals;
+}
+
+function getAllCourseFieldsLocations (order) {
   const courseFieldsLocations = [];
   _.forEach(DOCKET_COURSE_FIELDS, course => {
     const locations = _.reduce(order, (acc, val, index, coll) => {
@@ -187,27 +262,5 @@ function orderToObjectLiteral (order) {
   // console.log('courseFieldsLocations: ', courseFieldsLocations );
   // console.log('trimmedLocations: ', trimmedLocations);
 
-  // start building out the meal
-  // go thru trimmedLocations and take slices from 'order' variable, for
-  // a given course field
-  _.forEach(trimmedLocations,(val, index, coll) => {
-    const currentCourse = val[0];
-    const currentCourseFieldIndex = val[1][0];
-    var items;
-    if (index === trimmedLocations.length - 1) {
-      items = order.slice(currentCourseFieldIndex + 1, order.length); 
-    } else {
-      const nextCourseFieldIndex = trimmedLocations[index+1][1][0];
-      items = order.slice(currentCourseFieldIndex + 1, nextCourseFieldIndex); 
-    }
-
-    const parsedItems = _.map(items, item => {
-      var splitItem = item.split(/\s+/); // ["3","porterhouse","200"]
-      var itemQuantity = splitItem[0];
-      var itemName = splitItem.slice(1,splitItem.length).join(' ');
-      return {quantity: itemQuantity, item: itemName};
-    });
-    template.meals[currentCourse] = parsedItems;
-  });
-  dbHandler.insertSingleOrder(template);
+  return trimmedLocations;
 }
