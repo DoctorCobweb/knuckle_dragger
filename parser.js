363a77556dc2b5486491efa00628ccca4a7e7e25
dre <spinninghalf@gmail.com>
2018-08-20 22:00:35 +1000

    add: menu names as seen on physical dockets ive collected => needed for correctly segmenting items from their info section. A TODO

--- parser.js
+++ parser.js
@@ -37,6 +37,7 @@ function parseSingleOrderOfBytes (buffer) {
   nposParser.parse(buffer).then(function(ast) {
     npos.textualize(ast).then(function (results) {
       const data = sanitize(results);
+      console.log(colors.yellow(data));
       // data is an array of strings
       orderToObjectLiteral(data);
     }).catch(err => {
@@ -59,77 +60,6 @@ function sanitize(results) {
   return data;
 }
 
-/*
-function cleanData (results) {
-  // create a copy of results array
-  var data = _.slice(results);
-  var cleanedData_1 = _.reject(data, (s) => {
-    return (
-      s === '' || s === '\n' 
-    );
-  });
-  var cleanedData_2 = _.map(cleanedData_1, (s) => {
-    var tempp;
-    if (_.last(s) === '\n') {
-      tempp =  _.slice(s,0, s.length-1)
-    } else {
-      tempp = s;
-    }
-    return tempp.join("").toUpperCase();
-  });
-  return cleanedData_2;
-}
-*/
-
-/*
-// not really used at this stage. 
-// moreso for manually checking whether 'escpos-data-log.bin' 
-// contains the correct escpos data ie. it successfully parsers to
-// display many orders.
-function parseManyOrdersOfBytes (buffer) {
-  nposParser.parse(buffer).then(function(ast) {
-    npos.textualize(ast).then(function (results) {
-      var orderData = {};
-      const data = sanitize(results);
-      orderData['orders'] = splitIntoSingleOrders(data);
-      dbHandler.insertManyOrders(orderData)
-    }).catch(err => {
-      console.log('ERROR PARSER (textualize): '.red, err.message);
-    });
-  }).catch(err => {
-    console.log('ERROR PARSER (parser): '.red, err.message);
-  });
-}
-*/
-
-function splitIntoSingleOrders(data) {
-  var docketStartLocations = [];
-  // find start locations of orders
-  _.forEach(DOCKET_START_FIELDS, (field) => {
-    //console.log(field);
-    for (var i =0; i <= data.length; i++) {
-      // console.log( cleanedData_2[i]);
-      if (data[i] === field) {
-        // console.log(field, 'found start at: ', i);
-        docketStartLocations = _.concat(docketStartLocations, [i]);
-      }
-    }
-  });
-  // _.orderBy with no iteratees arg will sort by ascending order by default
-  var sortedDocketStartLocations = _.orderBy(docketStartLocations);
-  var orders = _.reduce(sortedDocketStartLocations, (acc, val, index, coll) => {
-    if (index === coll.length) {
-      var order = _.slice(data, sordedDocketStartLocations[index], data.length); 
-
-      //TODO check this return works... it had _concat(acc,[order]); (no period .)
-      return _.concat(acc, [order]);
-    } else {
-      var order = _.slice(data,sortedDocketStartLocations[index],sortedDocketStartLocations[index + 1]);
-      return _.concat(acc, [order]);
-    }
-  }, []);
-  return orders;
-}
 
 function orderToObjectLiteral (order) {
   //order will be an array of strings. but before going further check that the array
@@ -352,7 +282,12 @@ function buildOutMeals (order, trimmedLocations) {
       var splitItem = item.split(/\s+/); // ["3","porterhouse","200"]
       var itemQuantity = splitItem[0];
       var itemName = splitItem.slice(1,splitItem.length).join(' ');
-      return {quantity: itemQuantity, item: itemName};
+      var parsedItem = {};
+      parsedItem.quantity = itemQuantity;
+      parsedItem.item = itemName;
+      parsedItem.info = []; 
+
+      return parsedItem;
     });
     meals[currentCourse] = parsedItems;
   });
@@ -391,3 +326,35 @@ function getAllCourseFieldsLocations (order) {
 
   return trimmedLocations;
 }
+
+// ----------------------------------------
+/*
+function splitIntoSingleOrders(data) {
+  var docketStartLocations = [];
+  // find start locations of orders
+  _.forEach(DOCKET_START_FIELDS, (field) => {
+    //console.log(field);
+    for (var i =0; i <= data.length; i++) {
+      // console.log( cleanedData_2[i]);
+      if (data[i] === field) {
+        // console.log(field, 'found start at: ', i);
+        docketStartLocations = _.concat(docketStartLocations, [i]);
+      }
+    }
+  });
+  // _.orderBy with no iteratees arg will sort by ascending order by default
+  var sortedDocketStartLocations = _.orderBy(docketStartLocations);
+  var orders = _.reduce(sortedDocketStartLocations, (acc, val, index, coll) => {
+    if (index === coll.length) {
+      var order = _.slice(data, sordedDocketStartLocations[index], data.length); 
+
+      //TODO check this return works... it had _concat(acc,[order]); (no period .)
+      return _.concat(acc, [order]);
+    } else {
+      var order = _.slice(data,sortedDocketStartLocations[index],sortedDocketStartLocations[index + 1]);
+      return _.concat(acc, [order]);
+    }
+  }, []);
+  return orders;
+}
+*/
